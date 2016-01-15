206e840c552f78c1ba974003e81af0f2890a53ba
Kevin Fitzhenry <kevin.fitzhenry@createthenext.com>
2016-01-15 15:08:24 -0800

    Added functionality so user can add usernames in the command line

--- app.js
+++ app.js
@@ -1,4 +1,10 @@
 var profile = require("./profile.js");
 var username = "kevinfitzhenry";
+// var users = ["chalkers", "kevinfitzhenry"];
 
-profile.get(username);
+//Allows you to enter usernames in the terminal
+var users = process.argv.slice(2);
+
+// profile.get(username);
+
+users.forEach(profile.get);
